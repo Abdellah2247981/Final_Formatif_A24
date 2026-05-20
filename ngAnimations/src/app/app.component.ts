acfbfc3f8b4c13097e5dcc75f5a25065f5acf832
Mohamed Abdalla Tami <2247981@cegepmontpetit.ca>
2026-05-20 14:46:18 -0400

    FCT - Anime un fois avec angular

--- ngAnimations/src/app/app.component.ts
+++ ngAnimations/src/app/app.component.ts
@@ -1,16 +1,49 @@
 import { transition, trigger, useAnimation } from '@angular/animations';
 import { Component } from '@angular/core';
-import { bounce, shakeX, tada } from 'ng-animate';
+import { bounce, shake, shakeX, tada } from 'ng-animate';
 
 @Component({
-    selector: 'app-root',
-    templateUrl: './app.component.html',
-    styleUrls: ['./app.component.css'],
-    standalone: true
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger("shake", [
+      transition(":increment", useAnimation(shakeX, { params: { timing: 2 } })),
+    ]),
+    trigger("bounce", [
+      transition(":increment", useAnimation(bounce, { params: { timing: 4 } })),
+    ]),
+    trigger("tada", [
+      transition(":increment", useAnimation(tada, { params: { timing: 3 } })),
+    ])
+  ],
+  standalone: true
 })
 export class AppComponent {
   title = 'ngAnimations';
-
+  ng_shake = 0;
+  ng_bounce = 0;
+  ng_tada = 0;
   constructor() {
+  }
+
+
+  shakeFlipAndBounce_Angular_1() {
+
+    // Rouge immédiatement
+    this.ng_shake++;
+
+    // Vert après 2 sec
+    setTimeout(() => {
+      this.ng_bounce++;
+    }, 2000);
+
+    // Bleu 1 sec avant la fin du vert
+    // Vert dure 4 sec et commence à 2
+    // donc finit à 6
+    // bleu commence à 5
+    setTimeout(() => {
+      this.ng_tada++;
+    }, 5000);
   }
 }
